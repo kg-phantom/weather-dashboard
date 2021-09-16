130b45a146b4128376129dec4595ec273df2aa57
Kyrsten <ksurbayan@gmail.com>
2021-09-16 02:37:27 -0800

    display previous searches

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,8 @@ var submitHandler = function(event) {
     }
 
     getLatLong(cityName);
+
+    $("#city").val("");
 };
 
 var getLatLong = function(cityName) {
@@ -35,6 +37,10 @@ var getLatLong = function(cityName) {
                         searchHistory.splice(searchHistory.length - 1, 1);
                     }
                 }
+
+                if(searchHistory.length > 10) {
+                    searchHistory.splice(0, 1);
+                };
                 
                 localStorage.setItem("search history", JSON.stringify(searchHistory));
                 localStorage.setItem("last search", cityName);
@@ -56,19 +62,55 @@ var getForecast = function(cityLatitude, cityLongitude) {
                 var currentDate = dayjs.unix(data.current.dt).format("(M/D/YYYY)");
                 $("#current-date").text(currentDate);
 
-                console.log(dayjs.unix(data.daily[1].dt));
-
                 // present current weather
                 $("#current-temperature").text("Temp: " + data.current.temp + "°F");
                 $("#current-wind").text("Wind: " + data.current.wind_speed + " MPH");
                 $("#current-humidity").text("Humidity: " + data.current.humidity + "%");
-                $("#current-uv").text(data.current.uvi).addClass("rounded-3");
+                $("#current-uv").text(data.current.uvi).addClass("rounded-3 text-light");
 
+                if(data.current.uvi <= 2) {
+                    $("#current-uv").addClass("bg-success");
+                }
+                else if(data.current.uvi <= 5) {
+                    $("#current-uv").addClass("bg-warning");
+                }
+                else if(data.current.uvi <= 7) {
+                    $("#current-uv").addClass("bg-orange");
+                }
+                else {
+                    $("#current-uv").addClass("bg-danger");
+                };
+
+                // display current weather icon
                 var weatherIcon = data.current.weather[0].icon;
                 $("#current-icon").attr("src", "http://openweathermap.org/img/wn/" + weatherIcon + ".png");
+
+                for(var i = 1; i < 6; i++) {
+                    var forecastDayEl = $("#day" + i);
+                    var forecastDate = dayjs.unix(data.daily[i].dt).format("M/D/YYYY");
+                    forecastDayEl.find("h4").text(forecastDate);
+
+                    weatherIcon = data.daily[i].weather[0].icon;
+                    forecastDayEl.find("img").attr("src", "http://openweathermap.org/img/wn/" + weatherIcon + ".png");
+
+                    forecastDayEl.find("p.temp").text("Temp: " + data.daily[i].temp.day + "°F");
+
+                    forecastDayEl.find("p.wind").text("Wind: " + data.daily[i].wind_speed + " MPH");
+
+                    forecastDayEl.find("p.humidity").text("Humidity: " + data.daily[i].humidity + "%");
+                } 
             })
         }
     })
+};
+
+var displaySearches = function() {
+    for(var i = 0; i < searchHistory.length; i++) {
+        var searchHistoryButton = $("<button>").text(searchHistory[i]);
+        searchHistoryButton.addClass("w-100 btn p-1 mt-2");
+
+        $("#history-btns").append(searchHistoryButton);
+    }
 };
 
 if(!searchHistory) {
@@ -78,6 +120,12 @@ if(!searchHistory) {
 } else {
     // display weather for previous search
     getLatLong(localStorage.getItem("last search"));
-}
+};
+
+displaySearches();
 
 $("#submit-btn").on("click", submitHandler);
+$("#history-btns button").on("click", function(event) {
+    var cityName = event.target.textContent;
+    getLatLong(cityName);
+});
